bf576242415236fbc753fcabf44cb3c828aca8a0
olebergen <ole.bergen.92@gmail.com>
2025-03-19 10:00:31 +0100

    fix: jenkins build return

--- src/utils/jenkins.ts
+++ src/utils/jenkins.ts
@@ -7,13 +7,9 @@ import { print } from './log.ts';
 export const viewLatestBuild = async ({ fetch, job }: { fetch: Fetcher; job: string }) => {
   const builds = await fetch.get<{ builds: JenkinsBuild[] }>(new URL(job + config.jenkins.builds));
 
-  if (!Array.isArray(builds.builds) || builds.builds.length === 0) {
-    exitWithError('No builds found');
-  }
+  if (!Array.isArray(builds.builds) || builds.builds.length === 0) exitWithError('No builds found');
 
   const [latestBuild] = builds.builds;
 
-  if (latestBuild.result === 'SUCCESS' && latestBuild.url) {
-    print.info('view build at ' + latestBuild.url);
-  }
+  if (latestBuild.url) print.info('view build at ' + latestBuild.url);
 };
